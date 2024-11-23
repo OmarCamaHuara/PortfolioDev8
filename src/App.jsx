0ee2bc962cb168ac59625d4474c4aa8d66bce7ae
OmarCamaHuara <omar.js2023@gmail.com>
2024-11-23 04:42:20 -0300

    Section of the created contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Test from "./Test";
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero"
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Porfolio";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return <div>
@@ -12,15 +12,13 @@ const App = () => {
       <Navbar />
       <Hero />
     </section>
-    <section id="Services"><Parallax type="services"/></section>
-    <section><Services/></section>
-    <section id="Portfolio"><Parallax type="portfolio"/></section>
+    <section id="Services"><Parallax type="services" /></section>
+    <section><Services /></section>
+    <section id="Portfolio"><Parallax type="portfolio" /></section>
     <Portfolio />
-    <section>Portafolio 2</section>
-    <section>Portafolio 3</section>
-    <section id="Contact">Contact</section>
-    {/* <Test/>
-    <Test/> */}
+    <section id="contact">
+      <Contact />
+    </section>
   </div>;
 };
 
