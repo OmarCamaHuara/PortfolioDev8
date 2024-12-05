e5c82bc8891891a2ffe9c9618492ced4f9b69ae1
OmarCamaHuara <omar.js2023@gmail.com>
2024-12-05 17:44:52 -0300

    fixing url

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Hero from "./components/hero/Hero"
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Porfolio";
-import ChatBot from "./components/chatbot/Chatbot";
 import Contact from "./components/contact/Contact";
+import ChatBot from "./components/chatbot/ChatBot";
 import Cursor from "./components/cursor/Cursor";
 
 const App = () => {
